30148ddda8b5ff3df35a52577fac4438773f0b26
Xavier Morel <xmorel@prophesee.ai>
2024-12-12 23:28:08 +0100

    chore: copied some old helix config as comments for later

--- modules/home-manager/helix.nix
+++ modules/home-manager/helix.nix
@@ -20,7 +20,20 @@ in {
             display-messages = true;
             display-inlay-hints = true;
           };
+          # [editor.inline-diagnostics]
+          # cursor-line = "warning"
         };
+
+# [[language]]
+# name = "python"
+# language-servers = ["pylsp"]
+
+# [language-server.pylsp.config.pylsp]
+# plugins.pyls_mypy.enabled = true
+# plugins.pyls_mypy.live_mode = true
+      
+            
+                  
       };
     };
   };
