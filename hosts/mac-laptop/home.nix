d97623aa2e57715e0ea4c1f59d93f1e1659abd30
Xavier Morel <morelx42@gmail.com>
2025-07-02 17:22:23 +0200

    feat: update macbook config

--- hosts/mac-laptop/home.nix
+++ hosts/mac-laptop/home.nix
@@ -11,6 +11,10 @@
       FLAKE = "/Users/xmorel/workspace/nixos/";
     };
     packages = with pkgs; [
+      asciinema_3
+      aws-vault
+      docker-compose-language-service
+      pyright
       tree
     ];
     shell.enableFishIntegration = true;
@@ -18,10 +22,24 @@
 
   nix = {
     package = pkgs.nix;
-    settings.experimental-features = ["nix-command" "flakes"];
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = ["nix-command" "flakes"];
+      trusted-users = [
+        "root" "xmorel"
+      ];
+    };
+    gc = {
+      automatic = true;
+    };
+    extraOptions = ''
+      extra-substituters = https://devenv.cachix.org
+      extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
+    '';
   };
 
   programs = {
+    awscli.enable = true;
     bat = {
       enable = true;
       extraPackages = with pkgs.bat-extras; [ batdiff batman batwatch ];
@@ -50,10 +68,12 @@
     };
     fzf.enable = true;
     gh.enable = true;
+    gpg.enable = true;
     home-manager.enable = true;
     jujutsu.enable = true;
     nh.enable = true;
     nix-your-shell.enable = true;
+    password-store.enable = true;
     ripgrep.enable = true;
     starship.enable = true;
     yazi = {
